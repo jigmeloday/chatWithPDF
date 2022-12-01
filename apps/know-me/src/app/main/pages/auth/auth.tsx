1d79ee3a072c42483608bf350b073e98aecef788
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2022-12-02 02:39:29 +0600

    feat(app): route

--- apps/know-me/src/app/main/pages/auth/auth.tsx
+++ apps/know-me/src/app/main/pages/auth/auth.tsx
@@ -2,13 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
 const Login = lazy(() => import( '../auth/login/login' ));
-
+const SignUp = lazy(() => import('../auth/sign-up/sign-up'));
 export function Auth(): JSX.Element {
   return (
     <Suspense fallback='loading...'>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/sign-up' element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<Login />} />
         <Route path='*' element={<>page not found</>} />
       </Routes>
