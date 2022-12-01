4f4a40404f52ac05277fed2bdb25aa759fd61f80
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2022-12-02 02:58:24 +0600

    feat(app): folder

--- apps/know-me/src/app/main/pages/auth/forgot-password/forgot-password.tsx
+++ apps/know-me/src/app/main/pages/auth/forgot-password/forgot-password.tsx
@@ -1,14 +1,7 @@
-import styles from './forgot-password.module.scss';
+import { ForgotPassword } from '@know-me/auth-component';
 
-/* eslint-disable-next-line */
-export interface ForgotPasswordProps {}
-
-export function ForgotPassword(props: ForgotPasswordProps): JSX.Element {
-  return (
-    <div className={styles['container']}>
-      <h1>Welcome to ForgotPassword!</h1>
-    </div>
-  );
+export function ForgotPasswordComponent(): JSX.Element {
+  return (<ForgotPassword/>);
 }
 
-export default ForgotPassword;
+export default ForgotPasswordComponent;
