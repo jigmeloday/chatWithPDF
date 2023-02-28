00ea5d9e7d19282bc59e47a8e7f5f58968ea4f4f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-28 21:13:23 +0600

    feat(route): route refactor

--- apps/know-me/src/app/main/pages/landing/landing.tsx
+++ apps/know-me/src/app/main/pages/landing/landing.tsx
@@ -3,33 +3,9 @@ import { Suspense } from 'react';
 import { RouteModel } from '../../shared/model/shared.model';
 
 export function Landing():JSX.Element {
-  const isAdmin = false;
-
-  const ADMIN: RouteModel[] = [
-    {
-      id: '1',
-      path: '*',
-      component: <Landing />
-    }
-  ];
-  const USER: RouteModel[] = [
-    {
-      id: '1',
-      path: '*',
-      component: <Landing />
-    }
-  ];
-
+  
   return (
-    <Suspense fallback='loading...'>
-      <Routes>
-        {
-          (isAdmin ? ADMIN : USER).map(({ id, path, component }) =>
-            <Route key={`${path}+${id}`} path={path} element={component} />
-          )
-        }
-      </Routes>
-    </Suspense>
+   <>hello</>
   );
 }
 
