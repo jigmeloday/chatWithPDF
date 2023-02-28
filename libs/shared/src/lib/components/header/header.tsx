abcef19675a99b913189dd85a8c0a2cfb78c89cc
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-01 00:13:38 +0600

    feat(app): typography theme and style utilities

--- libs/shared/src/lib/components/header/header.tsx
+++ libs/shared/src/lib/components/header/header.tsx
@@ -4,19 +4,21 @@ import Typography from '../typography/typography';
 import { theme } from '../../theme/theme';
 import styles from './header.module.scss';
 
-export function Header(): JSX.Element {
+export function Header(props: { img: string }): JSX.Element {
   return (
     <Grid container item>
       <Grid item container xs={12} bgcolor={theme('light').palette.black.dark} py='6px'>
        <Grid item container direction='row' gap='12px' justifyContent='center'>
-         <Typography className={styles['blinking']} label='Free Shipping' variant='body1'/>
+         <Typography className={styles['blinking']} label='Free shipping' variant='body1'/>
          <Typography label='order from all item' variant='body1' color={theme('light').palette.white.main} />
        </Grid>
       </Grid>
       <Grid item container direction='column' px='48px' py='28px'>
         <Grid item container direction='row' px='28px'>
           <Grid item container xs={4}>
-            Logo
+            <Box width='90px' height='60px'>
+              <img src={props?.img} className='object-fit' />
+            </Box>
           </Grid>
           <Grid item container  xs={4} justifyContent='center' >
             <Input />
