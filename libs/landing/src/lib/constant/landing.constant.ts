d119e7c0407ed172e65bb0f594d7e3f687bea949
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-28 21:30:27 +0600

    feat(landing): landing constants

---
+++ libs/landing/src/lib/constant/landing.constant.ts
@@ -0,0 +1,18 @@
+import { Banner } from '../model/landing.model';
+
+export const BANNER: Banner[] =[
+  {
+    id: '',
+    title: '',
+    subTitle: '',
+    img: '',
+    url: ''
+  },
+  {
+    id: '',
+    title: '',
+    subTitle: '',
+    img: '',
+    url: ''
+  }
+];
