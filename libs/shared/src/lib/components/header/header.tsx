6403b5902fe242c9a0c799c38de8a9f6b07f6e1e
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-02-28 21:53:55 +0600

    feat(header): layouting

--- libs/shared/src/lib/components/header/header.tsx
+++ libs/shared/src/lib/components/header/header.tsx
@@ -1,13 +1,24 @@
-import styles from './header.module.scss';
+import { Grid } from '@mui/material';
+import Input from '../input/input';
 
-/* eslint-disable-next-line */
-export interface HeaderProps {}
-
-export function Header(props: HeaderProps) {
+export function Header(): JSX.Element {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Header!</h1>
-    </div>
+    <Grid container item direction='column' px='48px' py='28px'>
+      <Grid item container direction='row' px='28px'>
+        <Grid item container xs={4}>
+          Logo
+        </Grid>
+        <Grid item container  xs={4} justifyContent='center' >
+          <Input />
+        </Grid>
+        <Grid item container xs={4} justifyContent='end'>
+          hello
+        </Grid>
+      </Grid>
+      <Grid item container px='28px'>
+hi
+      </Grid>
+    </Grid>
   );
 }
 
