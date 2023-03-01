78268c34fd8ffd80375657f7e15b1ee4ed064d4c
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-01 18:03:20 +0600

    feat(header): profile menu

--- libs/shared/src/lib/components/header/components/cart.component.tsx
+++ libs/shared/src/lib/components/header/components/cart.component.tsx
@@ -1,44 +1,48 @@
 import { memo } from 'react';
-import { Avatar, Divider, ListItemIcon, Menu, MenuItem } from '@mui/material';
-import { Logout, PersonAdd, Settings } from '@mui/icons-material';
+import { Box, Grid, Menu } from '@mui/material';
 import { ProfileMenuProps } from '../model/header.model';
+import { Button } from '../../button/button';
+import Typography from '../../typography/typography';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
 
-function CartMenuComponent(props: ProfileMenuProps) {
+function CartMenuComponent(props: ProfileMenuProps): JSX.Element {
   return(
       <Menu
       anchorEl={props.anchorEl}
       id="account-menu"
       open={!!props.anchorEl}
       onClose={props.handleClose}
-      onClick={props.handleClose}
       transformOrigin={{ horizontal: 'right', vertical: 'top' }}
       anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
     >
-      <MenuItem onClick={props.handleClose}>
-        <Avatar /> Profile
-      </MenuItem>
-      <MenuItem onClick={props.handleClose}>
-        <Avatar /> My accountß
-      </MenuItem>
-      <Divider />
-      <MenuItem onClick={props.handleClose}>
-        <ListItemIcon>
-          <PersonAdd fontSize="small" />
-        </ListItemIcon>
-        Add another account
-      </MenuItem>
-      <MenuItem onClick={props.handleClose}>
-        <ListItemIcon>
-          <Settings fontSize="small" />
-        </ListItemIcon>
-        Settings
-      </MenuItem>
-      <MenuItem onClick={props.handleClose}>
-        <ListItemIcon>
-          <Logout fontSize="small" />
-        </ListItemIcon>
-        Logout
-      </MenuItem>
+        <Grid item container direction='column' px='24px'>
+          <Grid item container alignItems='center' direction='row' gap='18px'>
+            <Box width='80px' height='80px'>
+              <img src='https://images.unsplash.com/photo-1490885578174-acda8905c2c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZydWl0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+                height='100%' width='100%' className='object-fit--cover'
+              />
+            </Box>
+            <Box width='120px' >
+              <Typography label='Pineapple 100% Organic' variant='body1' />
+              <Grid item container direction='row' py='8px'>
+                <Box  height='20px' border='1px solid black' padding='2px'>
+                  <AddOutlinedIcon />
+                </Box>
+                <Box padding='2px' height='20px' border='1px solid black' className='text--center'  width='26px'>
+                  4
+                </Box>
+                <Box  height='20px' border='1px solid black' padding='2px'>
+                  <RemoveOutlinedIcon />
+                </Box>
+              </Grid>
+            </Box>
+            <Box>
+              <Typography label='4*120' variant='body1' />
+            </Box>
+          </Grid>
+          <Button />
+        </Grid>
     </Menu>
   );
 }
