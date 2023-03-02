1626e5b8a87fae1100a268c4da4b187b00ea7e10
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-02 21:49:13 +0600

    feat(banner): slider

--- libs/banner/src/lib/banner.style.ts
+++
@@ -1,8 +0,0 @@
-import { Grid, styled } from '@mui/material';
-
-export const BannerComponent = styled( Grid )(
-  ( { theme, ...props } ) => `
-      height: 86vh
-      
-  `
-);
--- libs/banner/src/lib/banner.tsx
+++ libs/banner/src/lib/banner.tsx
@@ -1,9 +1,9 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Autoplay, EffectFade, Navigation, Pagination, Scrollbar } from 'swiper';
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper';
 import { BANNER_DUMMY } from './constant/banner.constnat';
 import { Box, Grid } from '@mui/material';
-import { BannerComponent } from './banner.style';
-
+import { theme, Typography } from '@know-me/shared';
+import style from './banner.module.scss';
 
 export function Banner(): JSX.Element {
   return (
@@ -20,19 +20,32 @@ export function Banner(): JSX.Element {
       {
         BANNER_DUMMY.map(({ img, title, subTitle, id }) =>(
           <SwiperSlide>
-            <BannerComponent className='banner-component' item container >
-              <Box width='100%' className='position--relative'>
-                <img
-                  src={img}
-                  height='100%'
-                  width='100%'
-                  className='object-fit--cover '
-                />
-                <Grid item container className='banner-detail'>
-                  xccxcasdfasfdasfsadfasdfasdf
-                </Grid>
-              </Box>
-            </BannerComponent>
+            {/*<BannerComponent className='banner-component' item container >*/}
+            {/*  <Box width='100%' className='landing-cover position--relative'>*/}
+            {/*    <img*/}
+            {/*      src={img}*/}
+            {/*      height='100%'*/}
+            {/*      width='100%'*/}
+            {/*      className='object-fit--cover '*/}
+            {/*    />*/}
+            {/*    <Grid item container className='landing-cover__detail'>*/}
+            {/*      <Typography label='The UAE’s First Live Shopping Platform'*/}
+            {/*                  variant={ 'h2' }/>*/}
+            {/*    </Grid>*/}
+            {/*  </Box>*/}
+            {/*</BannerComponent>*/}
+            <Box className={ `${style['landing-cover']} position--relative`}>
+              <img height='100%' width='100%' className='object-fit--cover br-8'
+                   src={ img } alt='landing cover'/>
+
+              <Grid lg={ 6 } xs={9} className={style['landing-cover__detail']}>
+                <Typography label='The UAE’s First Live Shopping Platform'
+                            variant={ 'h2' }
+                            />
+                <Typography label='Introducing the whole new experience of shopping online' variant='body1'
+                            fontWeight={ theme('light').typography.fontWeightBold } />
+              </Grid>
+            </Box>
           </SwiperSlide>
         ))
       }
