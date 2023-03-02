5c14a8effa1394f4e6fc87f60b22cc6fa8f5bee0
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-02 13:03:03 +0600

    feat(header): card menu

---
+++ libs/banner/src/lib/constant/banner.constnat.ts
@@ -0,0 +1,10 @@
+import { BannerModel } from '../model/banner.model';
+
+export const BANNER_DUMMY: BannerModel[] = [
+  {
+    id: '1',
+    title: 'hello',
+    subTitle: 'hi',
+    img: ''
+  }
+];
---
+++ libs/banner/src/lib/model/banner.model.ts
@@ -0,0 +1,6 @@
+export interface BannerModel {
+  id: string,
+  img: string,
+  title: string,
+  subTitle: string
+}
