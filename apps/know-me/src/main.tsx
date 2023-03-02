8345070c1b543e185193fd63e1c9ab6386f7c516
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-02 16:37:52 +0600

    feat(banner): slider

--- apps/know-me/src/main.tsx
+++ apps/know-me/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css';
 import { BrowserRouter } from 'react-router-dom';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { store } from '@know-me/services';
--- libs/banner/src/lib/banner.tsx
+++ libs/banner/src/lib/banner.tsx
@@ -1,10 +1,22 @@
-import styles from './banner.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Autoplay, EffectFade, Navigation, Pagination, Scrollbar } from 'swiper';
+
 
 export function Banner(): JSX.Element {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Banner!</h1>
-    </div>
+    <Swiper
+      spaceBetween={50}
+      modules={ [ Autoplay, Pagination, EffectFade ] } effect={ 'fade' }
+      slidesPerView={ 1 }
+      autoplay={{ delay: 1000, disableOnInteraction: false }}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   );
 }
 
