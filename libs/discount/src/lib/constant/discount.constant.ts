67f40882645e5282606c1c245f88d4e304ecd514
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-05 20:24:29 +0600

    feat(discount): layout

---
+++ libs/discount/src/lib/constant/discount.constant.ts
@@ -0,0 +1,18 @@
+import { DiscountModel } from '../model/discount.model';
+
+export const DISCOUNT_DUMMY: DiscountModel[] = [
+  {
+    id: '1d',
+    title: 'test',
+    discount: 20,
+    url: '',
+    bgImage: 'https://vegina-store.myshopify.com/collections/cat2'
+  },
+  {
+    id: '2d',
+    title: 'test',
+    discount: 30,
+    url: '',
+    bgImage: 'https://cdn.shopify.com/s/files/1/0412/8151/9765/files/banner-1.jpg?v=1671871955'
+  }
+];
--- libs/discount/src/lib/discount.tsx
+++ libs/discount/src/lib/discount.tsx
@@ -1,9 +1,17 @@
 import { Grid } from '@mui/material';
+import { Card } from '@know-me/shared';
+import { DISCOUNT_DUMMY } from './constant/discount.constant';
 
 export function Discount():JSX.Element {
   return (
-    <Grid>
-
+    <Grid container item padding='32px'>
+      <Grid item container direction='row' px='26px' py='14px'>
+        {
+          DISCOUNT_DUMMY.map(({ discount, url, title, id, bgImage }) => (
+            <Card />
+          ))
+        }
+      </Grid>
     </Grid>
   );
 }
