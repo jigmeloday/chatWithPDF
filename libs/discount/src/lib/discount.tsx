d5179bdb60c90105ebbccf3d631416b772198050
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-03-07 14:54:43 +0600

    feat(discount): cart

--- libs/discount/src/lib/discount.tsx
+++ libs/discount/src/lib/discount.tsx
@@ -5,13 +5,18 @@ import { DISCOUNT_DUMMY } from './constant/discount.constant';
 export function Discount():JSX.Element {
   return (
     <Grid container item padding='32px'>
-      <Grid item container direction='row' px='26px' py='14px' justifyContent='space-around' >
+      <Grid item container direction='row' px='26px' py='34px' justifyContent='space-around' >
         {
           DISCOUNT_DUMMY.map(({ discount, url, title, id, bgImage }) => (
             <Grid item container xs={5.9}>
               <CustomCard>
-                <Grid item container xs={5}>
-                  <img src={bgImage}  className='object-fit--cover'/>
+                <Grid item container>
+                  <Grid item container xs={5} sx={{ position: 'relative'  }}>
+                    <img src={bgImage} className='object-fit--cover'/>
+                  </Grid>
+                  <Grid item container xs={5} sx={{ position: 'absolute'}}>
+                    hello
+                  </Grid>
                 </Grid>
               </CustomCard>
             </Grid>
