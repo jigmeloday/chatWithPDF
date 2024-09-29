bc42e280ab1a1a53fa8731cc625df7926a22fb96
Jigme Lodey <jigmenlodey@gmail.com>
2024-09-29 12:24:31 +0600

    pdf render

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -27,7 +27,7 @@ export const ourFileRouter = {
                     key: file.key,
                     name: file.name,
                     userId: metadata.userId,
-                    url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+                    url: `https://utfs.io/f/${file.key}`,
                     uploadStatus: 'PROCESSING',
                 }
             })
--- src/components/pdf-render.tsx
+++ src/components/pdf-render.tsx
@@ -7,32 +7,33 @@ import {
     RotateCw,
     Search,
 } from 'lucide-react'
-// import { Document, Page, pdfjs } from 'react-pdf'
+import { Document, Page, pdfjs } from 'react-pdf'
 import { useResizeDetector } from 'react-resize-detector'
-// import 'react-pdf/dist/Page/AnnotationLayer.css'
-// import 'react-pdf/dist/Page/TextLayer.css'
+import 'react-pdf/dist/Page/AnnotationLayer.css'
+import 'react-pdf/dist/Page/TextLayer.css'
 import { Button } from './ui/button'
-// import { Input } from './ui/input'
-// import { useForm } from 'react-hook-form'
+import { Input } from './ui/input'
 import { z } from 'zod'
-// import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { cn } from '@/lib/utils'
-// import {
-//     DropdownMenu,
-//     DropdownMenuContent,
-//     DropdownMenuItem,
-//     DropdownMenuTrigger,
-// } from './ui/dropdown-menu'
-// import SimpleBar from 'simplebar-react'
-// import PdfFullscreen from './PdfFullscreen'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import SimpleBar from 'simplebar-react'
+import 'simplebar-react/dist/simplebar.min.css'
 import { PdfRendererProps } from '@/components/model/max-width-wrapper';
+import { useToast } from '@/hooks/use-toast';
+import PdfFullscreen from '@/components/pdf-fullscreen';
 
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
 
 
 function PdfRenderer({ url }: PdfRendererProps) {
-    // const { toast } = useToast()
-
+    const { toast } = useToast()
     const [numPages, setNumPages] = useState<number>()
     const [currPage, setCurrPage] = useState<number>(1)
     const [scale, setScale] = useState<number>(1)
@@ -55,178 +56,142 @@ function PdfRenderer({ url }: PdfRendererProps) {
         typeof CustomPageValidator
         >
 
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     formState: { errors },
-    //     setValue,
-    // } = useForm<TCustomPageValidator>({
-    //     defaultValues: {
-    //         page: '1',
-    //     },
-    //     resolver: zodResolver(CustomPageValidator),
-    // })
-
-    // console.log(errors)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+    } = useForm<TCustomPageValidator>({
+        defaultValues: {
+            page: '1',
+        },
+        resolver: zodResolver(CustomPageValidator),
+    })
 
     const { width, ref } = useResizeDetector()
 
-    // const handlePageSubmit = ({
-    //                               page,
-    //                           }: TCustomPageValidator) => {
-    //     setCurrPage(Number(page))
-    //     setValue('page', String(page))
-    // }
+    const handlePageSubmit = ({ page, }: TCustomPageValidator) => {
+        setCurrPage(Number(page))
+        setValue('page', String(page))
+    }
 
     return (
         <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
             <div className='h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2'>
-                <div className='flex items-center gap-1.5'>
+                <div className='flex justify-center items-center gap-1.5'>
                     <Button
-                        disabled={currPage <= 1}
+                        disabled={currPage <= 1 }
                         onClick={() => {
-                            setCurrPage((prev) =>
-                                prev - 1 > 1 ? prev - 1 : 1
-                            )
-                            // setValue('page', String(currPage - 1))
+                            setCurrPage( ( prev ) => prev - 1 > 1 ? prev - 1 : 1 )
+                            setValue('page', String(currPage-1))
                         }}
-                        variant='ghost'
-                        aria-label='previous page'>
+                        variant='ghost' aria-label='previous page'>
                         <ChevronDown className='h-4 w-4' />
                     </Button>
-
-                    {/*<div className='flex items-center gap-1.5'>*/}
-                    {/*    <Input*/}
-                    {/*        {...register('page')}*/}
-                    {/*        className={cn(*/}
-                    {/*            'w-12 h-8',*/}
-                    {/*            errors.page && 'focus-visible:ring-red-500'*/}
-                    {/*        )}*/}
-                    {/*        onKeyDown={(e) => {*/}
-                    {/*            if (e.key === 'Enter') {*/}
-                    {/*                handleSubmit(handlePageSubmit)()*/}
-                    {/*            }*/}
-                    {/*        }}*/}
-                    {/*    />*/}
-                    {/*    <p className='text-zinc-700 text-sm space-x-1'>*/}
-                    {/*        <span>/</span>*/}
-                    {/*        <span>{numPages ?? 'x'}</span>*/}
-                    {/*    </p>*/}
-                    {/*</div>*/}
-
-                    {/*<Button*/}
-                    {/*    disabled={*/}
-                    {/*        numPages === undefined ||*/}
-                    {/*        currPage === numPages*/}
-                    {/*    }*/}
-                    {/*    onClick={() => {*/}
-                    {/*        setCurrPage((prev) =>*/}
-                    {/*            prev + 1 > numPages! ? numPages! : prev + 1*/}
-                    {/*        )*/}
-                    {/*        setValue('page', String(currPage + 1))*/}
-                    {/*    }}*/}
-                    {/*    variant='ghost'*/}
-                    {/*    aria-label='next page'>*/}
-                    {/*    <ChevronUp className='h-4 w-4' />*/}
-                    {/*</Button>*/}
+                    <div className='flex items-center gap-1.5'>
+                        <Input {...register('page')}
+                               onKeyDown={(e) => {
+                                   if (e.key === 'Enter') {
+                                       handleSubmit(handlePageSubmit)()
+                                   }
+                               }}
+                               className={cn('w-12 h-8', errors.page && 'focus-visible:ring-red-500')} />
+                        <p className='text-zinc-500 text-sm space-x-1'>
+                            <span>/</span>
+                            <span>{numPages ?? 0}</span>
+                        </p>
+                    </div>
+                    <Button
+                        disabled={currPage === numPages || numPages === undefined }
+                        onClick={() => {
+                            setCurrPage( ( prev ) => prev + 1 > numPages! ? numPages! : prev + 1 )
+                            setValue('page', String(currPage+1))
+                        }}
+                        variant='ghost' aria-label='next page'>
+                        <ChevronUp className='h-4 w-4' />
+                    </Button>
                 </div>
+                <div className='space-x-2'>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                className='gap-1.5'
+                                aria-label='zoom'
+                                variant='ghost'>
+                                <Search className='h-4 w-4' />
+                                {scale * 100}%
+                                <ChevronDown className='h-3 w-3 opacity-50' />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem
+                                onSelect={() => setScale(1)}>
+                                100%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                                onSelect={() => setScale(1.5)}>
+                                150%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                                onSelect={() => setScale(2)}>
+                                200%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem
+                                onSelect={() => setScale(2.5)}>
+                                250%
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
 
-                {/*<div className='space-x-2'>*/}
-                {/*    <DropdownMenu>*/}
-                {/*        <DropdownMenuTrigger asChild>*/}
-                {/*            <Button*/}
-                {/*                className='gap-1.5'*/}
-                {/*                aria-label='zoom'*/}
-                {/*                variant='ghost'>*/}
-                {/*                <Search className='h-4 w-4' />*/}
-                {/*                {scale * 100}%*/}
-                {/*                <ChevronDown className='h-3 w-3 opacity-50' />*/}
-                {/*            </Button>*/}
-                {/*        </DropdownMenuTrigger>*/}
-                {/*        <DropdownMenuContent>*/}
-                {/*            <DropdownMenuItem*/}
-                {/*                onSelect={() => setScale(1)}>*/}
-                {/*                100%*/}
-                {/*            </DropdownMenuItem>*/}
-                {/*            <DropdownMenuItem*/}
-                {/*                onSelect={() => setScale(1.5)}>*/}
-                {/*                150%*/}
-                {/*            </DropdownMenuItem>*/}
-                {/*            <DropdownMenuItem*/}
-                {/*                onSelect={() => setScale(2)}>*/}
-                {/*                200%*/}
-                {/*            </DropdownMenuItem>*/}
-                {/*            <DropdownMenuItem*/}
-                {/*                onSelect={() => setScale(2.5)}>*/}
-                {/*                250%*/}
-                {/*            </DropdownMenuItem>*/}
-                {/*        </DropdownMenuContent>*/}
-                {/*    </DropdownMenu>*/}
+                    <Button
+                        onClick={() => setRotation((prev) => prev + 90)}
+                        variant='ghost'
+                        aria-label='rotate 90 degrees'>
+                        <RotateCw className='h-4 w-4' />
+                    </Button>
 
-                {/*    <Button*/}
-                {/*        onClick={() => setRotation((prev) => prev + 90)}*/}
-                {/*        variant='ghost'*/}
-                {/*        aria-label='rotate 90 degrees'>*/}
-                {/*        <RotateCw className='h-4 w-4' />*/}
-                {/*    </Button>*/}
-
-                {/*    <PdfFullscreen fileUrl={url} />*/}
-                {/*</div>*/}
+                    <PdfFullscreen fileUrl={url} />
+                </div>
             </div>
+            <div className='flex-1 w-full max-h-screen'>
+                <SimpleBar autoHide={false} className='max-h-[calc(100vh-10rem)]'>
+                    <div ref={ref}>
+                        <Document
+                            loading={<div className='flex justify-center'>
+                                <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                            </div>}
+                            onLoadError={() => toast({ title: 'Error loading pdf', description: 'Please try again later', variant: 'destructive' })}
+                            onLoadSuccess={({numPages}) => setNumPages(numPages)}
+                            file={url} className='max-h-full mt-[10px]' >
+                            {isLoading && renderedScale ? (
+                                <Page
+                                    width={width ? width : 1}
+                                    pageNumber={currPage}
+                                    scale={scale}
+                                    rotate={rotation}
+                                    key={'@' + renderedScale}
+                                />
+                            ) : null}
 
-            {/*<div className='flex-1 w-full max-h-screen'>*/}
-            {/*    <SimpleBar*/}
-            {/*        autoHide={false}*/}
-            {/*        className='max-h-[calc(100vh-10rem)]'>*/}
-            {/*        <div ref={ref}>*/}
-            {/*            <Document*/}
-            {/*                loading={*/}
-            {/*                    <div className='flex justify-center'>*/}
-            {/*                        <Loader2 className='my-24 h-6 w-6 animate-spin' />*/}
-            {/*                    </div>*/}
-            {/*                }*/}
-            {/*                onLoadError={() => {*/}
-            {/*                    toast({*/}
-            {/*                        title: 'Error loading PDF',*/}
-            {/*                        description: 'Please try again later',*/}
-            {/*                        variant: 'destructive',*/}
-            {/*                    })*/}
-            {/*                }}*/}
-            {/*                onLoadSuccess={({ numPages }) =>*/}
-            {/*                    setNumPages(numPages)*/}
-            {/*                }*/}
-            {/*                file={url}*/}
-            {/*                className='max-h-full'>*/}
-            {/*                {isLoading && renderedScale ? (*/}
-            {/*                    <Page*/}
-            {/*                        width={width ? width : 1}*/}
-            {/*                        pageNumber={currPage}*/}
-            {/*                        scale={scale}*/}
-            {/*                        rotate={rotation}*/}
-            {/*                        key={'@' + renderedScale}*/}
-            {/*                    />*/}
-            {/*                ) : null}*/}
-
-            {/*                <Page*/}
-            {/*                    className={cn(isLoading ? 'hidden' : '')}*/}
-            {/*                    width={width ? width : 1}*/}
-            {/*                    pageNumber={currPage}*/}
-            {/*                    scale={scale}*/}
-            {/*                    rotate={rotation}*/}
-            {/*                    key={'@' + scale}*/}
-            {/*                    loading={*/}
-            {/*                        <div className='flex justify-center'>*/}
-            {/*                            <Loader2 className='my-24 h-6 w-6 animate-spin' />*/}
-            {/*                        </div>*/}
-            {/*                    }*/}
-            {/*                    onRenderSuccess={() =>*/}
-            {/*                        setRenderedScale(scale)*/}
-            {/*                    }*/}
-            {/*                />*/}
-            {/*            </Document>*/}
-            {/*        </div>*/}
-            {/*    </SimpleBar>*/}
-            {/*</div>*/}
+                            <Page
+                                className={cn(isLoading ? 'hidden' : '')}
+                                width={width ? width : 1}
+                                pageNumber={currPage}
+                                scale={scale}
+                                rotate={rotation}
+                                key={'@' + scale}
+                                loading={
+                                    <div className='flex justify-center'>
+                                        <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                                    </div>
+                                }
+                                onRenderSuccess={() =>
+                                    setRenderedScale(scale)
+                                } />
+                        </Document>
+                    </div>
+                </SimpleBar>
+            </div>
         </div>
     )
 }
